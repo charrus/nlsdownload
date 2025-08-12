92f435eb966776d87ff67a8e865391ae6f160460
Charlie Rusbridger <charlie@rusbridger.com>
2025-08-12 11:52:40 +0100

    Use aiofiles, show progress with #, use TemporaryDir and Pathlib

--- iif2.py
+++ iif2.py
@@ -6,10 +6,10 @@ import os
 import time
 
 import httpx
-from aiofile import async_open
+import aiofiles
 from PIL import Image
 
-# from pathlib import Path
+from pathlib import Path
 
 
 QUEUE_SIZE = 8
@@ -18,14 +18,16 @@ QUEUE_SIZE = 8
 async def consumer(queue, client):
     while True:
         tile = await queue.get()
-        print(f"Requesting: {tile['url']}")
         r = await client.get(tile["url"])
-        print(
-            f"Got status code: {r.status_code} for {tile['url']} => {tile['file']}"
-        )
         if r.status_code == 200:
-            async with async_open(tile["file"], mode="wb") as img:
+            await aiofiles.stderr.write("#")
+            async with aiofiles.open(tile["file"], mode="wb") as img:
                 await img.write(r.content)
+        else:
+            await aiofiles.stderr.write("!")
+
+        await aiofiles.stderr.flush()
+
         queue.task_done()
 
 
@@ -58,56 +60,58 @@ async def main():
     else:
         output_path = f"{path}.{img_type}"
 
-    async with httpx.AsyncClient(http2=True) as client:
-        r = await client.get(imageurl)
-        if r.status_code != 200:
-            print(f"Error fetching image info: {r.status_code}")
-            return
-        image_data = r.json()
-        base_url = image_data.get("id", image_data.get("@id"))
-        path = base_url.split("/")[-1]
-        tile_width = image_data["tiles"][0]["width"]
-        tile_height = image_data["tiles"][0]["height"]
-        # Pick the smallest scale factor(usually 1x)
-        # This is to ensure we get the highest resolution tiles available
-        scale_factor = min(image_data["tiles"][0]["scaleFactors"])
-        # And pass the index of scale, as per the API.
-        scale = image_data["tiles"][0]["scaleFactors"].index(scale_factor)
-        tiles = []
-        tasks = []
+    print(f"Downloading tiles for f{imageurl}:")
+    async with aiofiles.tempfile.TemporaryDirectory() as tmpdir:
+        async with httpx.AsyncClient(http2=True) as client:
+            r = await client.get(imageurl)
+            if r.status_code != 200:
+                print(f"Error fetching image info: {r.status_code}")
+                return
+            image_data = r.json()
+            base_url = image_data.get("id", image_data.get("@id"))
+            path = base_url.split("/")[-1]
+            tile_width = image_data["tiles"][0]["width"]
+            tile_height = image_data["tiles"][0]["height"]
+            # Pick the smallest scale factor(usually 1x)
+            # This is to ensure we get the highest resolution tiles available
+            scale_factor = min(image_data["tiles"][0]["scaleFactors"])
+            # And pass the index of scale, as per the API.
+            scale = image_data["tiles"][0]["scaleFactors"].index(scale_factor)
+            tiles = []
+            tasks = []
 
-        for _ in range(QUEUE_SIZE):
-            tasks.append(asyncio.create_task(consumer(queue, client)))
+            for _ in range(QUEUE_SIZE):
+                tasks.append(asyncio.create_task(consumer(queue, client)))
 
-        for x in range(0, image_data["width"], tile_width):
-            for y in range(0, image_data["height"], tile_height):
-                tile = {"x": x, "y": y}
-                # The right-most and bottom-most tiles need to be decreased
-                # if they would exceed the size of the full image.
-                this_tile_width = min(tile_width, image_data["width"] - x)
-                this_tile_height = min(tile_height, image_data["height"] - y)
-                tile["file"] = tile_filename(path, img_type, x, y)
-                tile["url"] = tile_url(
-                    base_url, scale, img_type, x, y, this_tile_width, this_tile_height
-                )
-                tiles.append(tile)
-                queue.put_nowait(tile)
+            for x in range(0, image_data["width"], tile_width):
+                for y in range(0, image_data["height"], tile_height):
+                    tile = {"x": x, "y": y}
+                    # The right-most and bottom-most tiles need to be decreased
+                    # if they would exceed the size of the full image.
+                    this_tile_width = min(tile_width, image_data["width"] - x)
+                    this_tile_height = min(tile_height, image_data["height"] - y)
+                    tile["file"] = Path(tmpdir, tile_filename(path, img_type, x, y))
+                    tile["url"] = tile_url(
+                        base_url, scale, img_type, x, y, this_tile_width, this_tile_height
+                    )
+                    tiles.append(tile)
+                    queue.put_nowait(tile)
 
-        await queue.join()
+            await queue.join()
 
-        for task in tasks:
-            task.cancel()
+            for task in tasks:
+                task.cancel()
 
-        await asyncio.gather(*tasks, return_exceptions=True)
+            await asyncio.gather(*tasks, return_exceptions=True)
 
-    print(f"Creating montage {output_path}")
-    montage = Image.new("RGB", (image_data["width"], image_data["height"]))
-    for tile in tiles:
-        with Image.open(tile["file"]) as im:
-            montage.paste(im, (tile["x"], tile["y"]))
-        os.remove(tile["file"])
-    montage.save(output_path)
-    print(f"Montage saved to {output_path}")
+        print()
+        print(f"Creating montage {output_path}")
+        montage = Image.new("RGB", (image_data["width"], image_data["height"]))
+        for tile in tiles:
+            with Image.open(tile["file"]) as im:
+                montage.paste(im, (tile["x"], tile["y"]))
+        montage.save(output_path)
+        print(f"Montage saved to {output_path}")
 
 
 if __name__ == "__main__":
